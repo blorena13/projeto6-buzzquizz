ca6565b58d2de05a5099bfa08d64268b0e409e0c
micka <mickalarissa01@gmail.com>
2023-01-25 13:22:52 -0300

    Inicio da implementação da pagina2

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@ function runquizz(selecionado){
 
 const elementhearder = document.querySelector('.topo');
 const elementcont= document.querySelector('.container');
+const elementpage2= document.querySelector('.pagina2');
 
 // adicionar a classe escondido para montar a tela 2
-elementhearder.classList.add('escondido');
+//elementhearder.classList.add('escondido');(coloquei comentado pois o topo não desparece)
 elementcont.classList.add('escondido');
-
+elementpage2.classList.remove('escondido');
 }
