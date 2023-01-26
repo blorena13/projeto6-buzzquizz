3c2a3c27e4dfa6aaec5fe5f92d554641f086be0d
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-01-26 09:37:22 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 // ao clicar em algum dos quizz muda pra tela 2
 function runquizz(selecionado){
 
-const elementhearder = document.querySelector('.topo');
 const elementcont= document.querySelector('.container');
 const elementpage2= document.querySelector('.pagina2');
 
@@ -10,4 +9,12 @@ const elementpage2= document.querySelector('.pagina2');
 //elementhearder.classList.add('escondido');(coloquei comentado pois o topo não desparece)
 elementcont.classList.add('escondido');
 elementpage2.classList.remove('escondido');
+}
+//ao clicar no botao abrir a tela some e da inicio a tela 3
+
+function createquizz(){
+
+    const elementcont= document.querySelector('.container');
+// falta a parte da pagina 3 para selecionar e remover o escondido dela para trocar a tela
+    elementcont.classList.add('escondido');
 }
